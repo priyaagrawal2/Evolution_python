6ee792a6318ddb6c8a0316f864e9bca4c74b1d18
priyaagrawal2 <148893325+priyaagrawal2@users.noreply.github.com>
2025-04-09 02:45:50 +0530

    Update eda.py
    EDA completed

--- eda.py
+++ eda.py
@@ -22,3 +22,29 @@ print("Total null values: ",df.isnull().sum().sum())
 #Unique values present in dataset
 unique_values = df.nunique()
 print("Unique values present in dataset: ",unique_values)
+#Data Types 
+print("Data types present in dataset: ",df.dtypes)
+
+#Duplicate rows present in each column
+duplicate_rows = df.duplicated().sum()
+print("Duplicate rows present in dataset: ",duplicate_rows)
+
+#Outliers count that are present in dataset in column 'Time'
+col = 'Time'
+
+Q1 = df[col].quantile(0.25)
+Q3 = df[col].quantile(0.75)
+IQR = Q3 - Q1
+
+outlier_count = ((df[col] < Q1 - 1.5 * IQR) | (df[col] > Q3 + 1.5 * IQR)).sum()
+
+print("Outlier count:", outlier_count)
+
+
+
+#Correaltion matrix and heatmap
+correlation_matrix = df.corr(numeric_only = True)
+plt.figure(figsize=(8,6))
+sns.heatmap(correlation_matrix, annot = True, cmap = "Reds",fmt = ".2f",linewidths = 0.8)
+plt.title("Correlation Heatmap")
+plt.show()
